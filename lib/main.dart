baa98204105d7da3aa7390b8d7b4315f46152044
noah <noah85.Nam@gmail.com>
2021-04-23 20:40:57 +0900

    home화면에 스위치 구성

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  var switchValue = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Container (
-          color: Colors.white,
-          child: Center(
-          child: Text('hellow\nFlutter', textAlign: TextAlign.center, style: TextStyle(color: Colors.blue, fontSize: 20),)
-        )
-      )
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        darkTheme: ThemeData.light(),
+        home: Scaffold(
+            body: Center(
+                child: Switch(
+                    value: switchValue,
+                    onChanged: (value) {
+                      switchValue = value;
+                    }))));
   }
 }
